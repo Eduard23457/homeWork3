814a759af8ef2ed435edcb54d61cd8b9328b3b31
Eduard23457 <khidiyatov2000@bk.ru>
2024-07-12 20:32:28 +0500

    homeWork

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import com.sun.source.doctree.SystemPropertyTree;
+
+import javax.annotation.processing.SupportedSourceVersion;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}
